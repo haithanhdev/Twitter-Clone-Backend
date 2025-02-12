1aaa68e79d53d355f76d5e08946b7a0bd116af57
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-02-12 22:30:18 +0700

    demo register user

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -12,4 +15,23 @@ export const loginController = (req: Request, res: Response) => {
     error: 'Login failed'
   })
   return
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+    res.json({
+      message: 'Register success',
+      result: result
+    })
+    return
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+    return
+  }
 }
---
+++ src/services/users.services.ts
@@ -0,0 +1,19 @@
+import User from '~/models/schemas/User.schema'
+import databaseService from './database.services'
+
+class UsersService {
+  async register(payload: { email: string; password: string }) {
+    const { email, password } = payload
+    const result = await databaseService.users.insertOne(
+      new User({
+        email,
+        password
+      })
+    )
+    return result
+  }
+}
+
+//Tạo 1 instance của UsersService
+const usersService = new UsersService()
+export default usersService
