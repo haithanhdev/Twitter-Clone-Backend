4adbb9d27b705d88f0a95a4c6858616b001cc6dd
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-02-15 10:11:11 +0700

    Define user register requests type

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { RegisterReqBody } from '~/models/requests/User.requests'
 import User from '~/models/schemas/User.schema'
 import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
@@ -17,10 +19,9 @@ export const loginController = (req: Request, res: Response) => {
   return
 }
 
-export const registerController = async (req: Request, res: Response) => {
-  const { email, password } = req.body
+export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
   try {
-    const result = await usersService.register({ email, password })
+    const result = await usersService.register(req.body)
     res.json({
       message: 'Register success',
       result: result
