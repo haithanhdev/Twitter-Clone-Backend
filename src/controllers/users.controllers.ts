f6fa93d4e12db6e7ca54f1ad281f2e49139b944a
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-02-17 09:09:25 +0700

    create wrapRequestHandler

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { ParamsDictionary } from 'express-serve-static-core'
+import { NextFunction, ParamsDictionary } from 'express-serve-static-core'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import usersService from '~/services/users.services'
 
@@ -17,20 +17,14 @@ export const loginController = (req: Request, res: Response) => {
   return
 }
 
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  try {
-    const result = await usersService.register(req.body)
-    res.json({
-      message: 'Register success',
-      result: result
-    })
-    return
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({
-      message: 'Register failed',
-      error
-    })
-    return
-  }
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await usersService.register(req.body)
+  res.json({
+    message: 'Register success',
+    result: result
+  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 
@@ -7,9 +7,10 @@ const port = 3000
 
 app.use(express.json())
 app.use('/users', usersRouter)
-
 databaseService.connect()
-
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({ error: err.message })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
