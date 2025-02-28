8cf62798d05a8e187a6d3a7361acb758911f56ea
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-01 00:28:56 +0700

    init folder uploads

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 
 databaseService.connect()
 const app = express()
 const port = 4000
+
+//Tạo folder uploads
+initFolder()
 
 app.use(express.json())
 app.use('/users', usersRouter)
