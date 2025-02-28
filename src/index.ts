7ba060e165d507f91671336ee1f4b366ee30c12d
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-02-28 22:15:59 +0700

    Upload file by using formidable

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
 
 databaseService.connect()
 const app = express()
@@ -9,6 +10,7 @@ const port = 4000
 
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
