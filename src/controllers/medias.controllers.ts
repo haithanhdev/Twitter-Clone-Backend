773cb6ad38590655d032ec2b76073f3c7ef8bc3b
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-03 11:35:20 +0700

    Filter upload image

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,24 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
-import formidable from 'formidable'
+// import formidable from 'formidable'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  //Sử dụng câu lệnh sau nếu gặp lỗi require of ESModule
-  // const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024 * 1024 //300MB
+  const data = await handleUploadSingleImage(req)
+  res.json({
+    result: data
   })
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      fields,
-      files
-    })
-    return
-  })
+  return
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express'
 import fs from 'fs'
 import path from 'path'
 export const initFolder = () => {
@@ -7,4 +8,36 @@ export const initFolder = () => {
       recursive: true // mục đích là để tạo folder nested
     })
   }
+}
+
+export const handleUploadSingleImage = async (req: Request) => {
+  // Sử dụng câu lệnh sau nếu gặp lỗi require of ESModule
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 5,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024 * 1024, //300MB,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('Invalid file type') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('Image is required'))
+      }
+      resolve({
+        files
+      })
+    })
+  })
 }
