4aaa8f38471e9192bcf44dae527da2f030f72c9d
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-04 15:54:40 +0700

    Serving static file using express router method get

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 // import formidable from 'formidable'
 import path from 'path'
+import { UPLOAD_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 import { handleUploadSingleImage } from '~/utils/file'
@@ -10,6 +11,16 @@ export const uploadSingleImageController = async (req: Request, res: Response, n
   res.json({
     message: USERS_MESSAGES.UPLOAD_SUCCESS,
     result: url
+  })
+  return
+}
+
+export const serveImageController = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not Found')
+    }
   })
   return
 }
