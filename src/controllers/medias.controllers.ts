34cb05efd33212a37ac2247ae51f9ea47e440b29
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-04 22:56:17 +0700

    upload video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 // import formidable from 'formidable'
 import path from 'path'
-import { UPLOAD_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 
@@ -14,12 +14,31 @@ export const uploadImageController = async (req: Request, res: Response, next: N
   return
 }
 
+export const uploadVideoController = async (req: Request, res: Response, next: NextFunction) => {
+  const url = await mediasService.UploadVideo(req)
+  res.json({
+    message: USERS_MESSAGES.UPLOAD_SUCCESS,
+    result: url
+  })
+  return
+}
+
 export const serveImageController = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.params
-  res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+  res.sendFile(path.resolve(UPLOAD_IMAGE_DIR, name), (err) => {
     if (err) {
       res.status((err as any).status).send('Not Found')
     }
   })
   return
+}
+
+export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
+    // console.log('err', err)
+    if (err) {
+      res.status((err as any).status).send('Not Found')
+    }
+  })
 }
