e13143bb7d8ed27908ab2a262690e447efc06ca5
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-05 09:39:22 +0700

    Customize streaming video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
 // import formidable from 'formidable'
 import path from 'path'
+import fs from 'fs'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
+import HTTP_STATUS from '~/constants/httpStatus'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
+import mime from 'mime'
 
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
   const url = await mediasService.UploadImage(req)
@@ -33,12 +36,56 @@ export const serveImageController = (req: Request, res: Response, next: NextFunc
   return
 }
 
-export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+export const serveVideoStreamController = (req: Request, res: Response, next: NextFunction) => {
+  const range = req.headers.range
+  if (!range) {
+    res.status(HTTP_STATUS.BAD_REQUEST).send('Missing Range Header')
+    return
+  }
   const { name } = req.params
-  res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
-    // console.log('err', err)
-    if (err) {
-      res.status((err as any).status).send('Not Found')
-    }
-  })
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name)
+  //1MB = 10^6 bytes (Tính theo hệ 10, đây là thứ mà chúng ta hay thấy trên UI)
+  //Còn nếu tính theo hệ nhị phân thì 1 MB = 2^20 bytes (1024 * 1024)
+
+  //Dung lượng video (bytes)
+  const videoSize = fs.statSync(videoPath).size
+  //Dung lượng video cho mỗi phân đoạn stream
+  const chunkSize = 10 ** 6 //1MB
+  //Lấy giá trị byte bắt đầu từ header range (vd: bytes=5931008-)
+  const start = Number(range.replace(/\D/g, '')) // Lấy ra giá trị bytes trên header range (vd: 5931008)
+  //Lấy giá trị byte kết thúc, vượt quá dung lượng video thì lấy giá trị videoSize
+  const end = Math.min(start + chunkSize, videoSize - 1)
+
+  //Dung lượng thực tế cho mỗi đoạn video stream
+  //Thường đây sẽ là chunkSize, ngoại trừ đoạn cuối cùng
+  const contentLength = end - start + 1
+  const contentType = mime.getType(videoPath) || 'video/*'
+
+  /**
+   * Format của header Content-Range: bytes <start>-<><end>/<videoSize>
+   * Ví dụ: Content-Range: bytes 0-4999999/10000000
+   * Yêu cầu là `end` phải luôn luôn nhỏ hơn `videoSize`
+   * ❌ 'Content-Range': 'bytes 0-100/100' => treo video
+   * ✅ 'Content-Range': 'bytes 0-99/100'
+   *
+   * Còn Content-Length sẽ là end - start + 1. Đại diện cho khoảng cách
+   * Để dễ hình dung, mọi người tưởng tượng từ số 0 đến số 10 thì ta có 11 số
+   * byte cũng tương tự nếu start = 0, end = 10 thì ta có 11 byte
+   * Công thức là end - start + 1
+   *
+   * ChunkSize = 50
+   * videoSize = 100
+   * |0-------------------50|51------------------99|100 (end)
+   * stream 1: start = 0, end = 50 => contentLength = 51
+   * stream 2: start = 51, end = 99 => contentLength = 49 (total = 100 ✅)
+   */
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+  res.writeHead(HTTP_STATUS.PARTIAL_CONTENT, headers)
+  const videoStreams = fs.createReadStream(videoPath, { start, end })
+  videoStreams.pipe(res)
 }
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
-import { UPLOAD_IMAGE_DIR } from './constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from './constants/dir'
 import staticRouter from './routes/static.routes'
 config()
 databaseService.connect()
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
 app.use('/static', staticRouter)
-// app.use('/static', express.static(UPLOAD_IMAGE_DIR))
+app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
 app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
