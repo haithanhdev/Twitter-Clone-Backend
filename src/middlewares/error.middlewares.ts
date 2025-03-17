ee065dcdbdec05ebd7e5457e60d42cf47cdae314
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-17 17:08:39 +0700

    Fix bug AWS key

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -4,16 +4,35 @@ import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/Errors'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    res.status(err.status).json(omit(err, ['status']))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      res.status(err.status).json(omit(err, ['status']))
+      return
+    }
+    //Sửa đổi thuộc tính của object err => Mong muốn xuất hiện khi JSON.stringify sử dụng enumrable: true
+    //Nếu thuộc tính có configurable: false thì không defineProperty => chuyển thành true thì OK
+    // console.log(Object.getOwnPropertyNames(err)) // dùng để check xem có bao nhiêu key
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      // console.log(Object.getOwnPropertyDescriptor(err, key)) //Dùng để check cụ thể từng key
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errorInfo: omit(finalError, 'stack')
+    })
+    return
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal Server Error',
+      errorInfo: omit(error as any, 'stack')
+    })
     return
   }
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, 'stack')
-  })
-  return
 }
