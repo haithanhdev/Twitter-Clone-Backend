77ea478603c9babd0287772123840ed047b2419c
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-17 08:47:34 +0700

    Search Validator

--- src/routes/search.routes.ts
+++ src/routes/search.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express'
 import { searchController } from '~/controllers/search.controllers'
+import { searchValidator } from '~/middlewares/search.middlewares'
+import { paginationValidator } from '~/middlewares/tweets.middlewares'
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 
 const searchRouter = Router()
 
-searchRouter.get('/', accessTokenValidator, verifiedUserValidator, searchController)
+searchRouter.get(
+  '/',
+  accessTokenValidator,
+  verifiedUserValidator,
+  paginationValidator,
+  searchValidator,
+  searchController
+)
 
 export default searchRouter
--- src/services/search.services.ts
+++ src/services/search.services.ts
@@ -1,7 +1,7 @@
 import { SearchQuery } from '~/models/requests/Search.requests'
 import databaseService from './database.services'
 import { ObjectId } from 'mongodb'
-import { MediaType, MediaTypeQuery, TweetType } from '~/constants/enums'
+import { MediaType, MediaTypeQuery, PeopleFollow, TweetType } from '~/constants/enums'
 
 class SearchService {
   async search({
@@ -16,7 +16,7 @@ class SearchService {
     page: number
     content: string
     user_id: string
-    people_follow?: string
+    people_follow?: PeopleFollow
     media_type?: MediaTypeQuery
   }) {
     const $match: any = {
@@ -35,7 +35,7 @@ class SearchService {
       }
     }
 
-    if (people_follow && people_follow === '1') {
+    if (people_follow && people_follow === PeopleFollow.Following) {
       const user_id_obj = new ObjectId(user_id)
       const followed_user_ids = await databaseService.followers
         .find(
